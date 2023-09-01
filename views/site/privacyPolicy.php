056a488e5202a6a075183b9413499fe0eb3b99ec
Aliya <aliya.genius@gmail.com>
2023-09-01 10:07:50 +0800

    Create Privacy Policy page

---
+++ views/site/privacyPolicy.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = 'Privacy Policy'; // Nama page
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+
+<div class="site-privacy-policy">
+    <h1><?= Html::encode($this->title); ?></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam ipsum vitae nisi vestibulum, sed convallis nisi sagittis. Curabitur eget pretium purus. In diam ligula, dictum ac blandit feugiat, auctor non odio. Nullam dapibus ligula purus, nec dictum ipsum iaculis quis. Praesent posuere arcu tempus gravida faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu urna a ante aliquet imperdiet nec eu metus. Mauris sodales nibh a velit commodo, at tristique ligula finibus.
+    </p> 
+
+    <p>
+        Phasellus viverra purus mi, ac tempus ex rhoncus et. Duis pretium, nisl at vestibulum tempus, metus purus tristique urna, quis finibus lectus leo eu libero. Ut at semper ante. Sed arcu sem, dapibus in posuere a, egestas et mi. Ut semper urna magna, sit amet luctus lacus semper vel. In luctus tortor et maximus euismod. Vivamus sodales malesuada massa, vitae pharetra nulla suscipit at. Nulla tincidunt posuere nisl, id dapibus leo auctor nec. Sed venenatis arcu lacus, vitae consectetur ipsum luctus eget. Cras nec enim vel velit commodo varius. Nulla interdum tortor non vestibulum pharetra. Quisque nisl nisi, elementum mattis vulputate sit amet, ultrices eget quam. Vivamus vel enim nec velit sodales ultricies. Vivamus in ipsum eu elit fermentum facilisis. Sed pellentesque purus ut enim laoreet rutrum. Nullam rhoncus sapien non ante lobortis, et aliquam purus vehicula.
+    </p>
+    
+    <p>
+        Sed neque dui, gravida lobortis arcu eget, eleifend venenatis nisl. Donec feugiat, nunc sit amet iaculis venenatis, nulla felis luctus massa, id dapibus ipsum risus quis urna. Aliquam odio mi, sollicitudin nec nunc nec, pharetra euismod augue. Pellentesque vel egestas nisl, et facilisis odio. Donec eu vulputate nisl. Aliquam commodo magna sit amet mauris aliquam aliquet. Praesent et tellus non est sagittis luctus in sit amet mi. Curabitur consectetur efficitur augue, in ullamcorper magna lacinia quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+
+    <p>
+        Nullam porta, nunc quis maximus commodo, nisi ligula suscipit leo, et euismod nibh urna at mauris. Fusce auctor tortor lectus, tempus interdum ex malesuada id. Nam commodo viverra fermentum. Integer lacinia laoreet dui, sed luctus velit faucibus vitae. Integer tristique mollis purus, eu accumsan elit iaculis et. Nam et dolor porta, elementum justo in, ultrices purus. Vestibulum in magna in sapien venenatis consequat. Suspendisse at lacinia magna. Ut ullamcorper dignissim libero, at facilisis ante interdum sed. Aliquam vitae felis non mi efficitur maximus. Aliquam posuere sit amet lorem quis maximus. Nulla tempor ultrices turpis vitae auctor. Curabitur fermentum, lacus sed lobortis ullamcorper, nisi elit pretium erat, quis facilisis metus felis a mauris. Fusce nisi purus, feugiat vel enim ut, vestibulum pharetra ex.
+    </p>
+
+    <p>
+        Integer ornare mollis lorem ut elementum. Fusce varius justo ac tortor sollicitudin congue. Etiam in massa id ligula tincidunt vestibulum non ac ligula. Vivamus facilisis magna id quam vulputate, at tincidunt quam feugiat. Nulla et elit tristique, viverra nibh sed, pharetra odio. Curabitur sed dolor sit amet neque vestibulum eleifend eget et eros. Nullam in consequat erat, eu fringilla elit. Maecenas semper mauris ut metus consequat rutrum. Ut velit mi, tempor nec mauris ut, consequat aliquam tortor. Phasellus non libero at dui hendrerit fringilla non eget urna. Duis ullamcorper mi ut enim accumsan tempus.
+    </p>
+</div>
